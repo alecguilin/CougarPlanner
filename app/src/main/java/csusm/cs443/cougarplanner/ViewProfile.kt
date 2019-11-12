1e90474c6a10d23a85bbb43e07cda419c483fdd6
WutangchickeN <34319000+WutangchickeN@users.noreply.github.com>
2019-11-12 08:28:46 -0800

    Add files via upload

---
+++ app/src/main/java/csusm/cs443/cougarplanner/ViewProfile.kt
@@ -0,0 +1,12 @@
+package csusm.cs443.cougarplanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProfile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile)
+    }
+}
---
+++ app/src/main/java/csusm/cs443/cougarplanner/ViewSingleClass.kt
@@ -0,0 +1,12 @@
+package csusm.cs443.cougarplanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewSingleClass : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_single_class)
+    }
+}
