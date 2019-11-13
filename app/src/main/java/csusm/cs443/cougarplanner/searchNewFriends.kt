a7b831b70dd892ce8e6a63f7442c381d5650cf9e
WutangchickeN <34319000+WutangchickeN@users.noreply.github.com>
2019-11-13 08:25:51 -0800

    Add files via upload

---
+++ app/src/main/java/csusm/cs443/cougarplanner/searchNewFriends.kt
@@ -0,0 +1,12 @@
+package csusm.cs443.cougarplanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class searchNewFriends : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_new_friends)
+    }
+}
