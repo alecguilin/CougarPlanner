1fe4cb5e5fe3e07cc1148707fe1f9f88e50ca167
Joseph Santibout <santi092@cougars.csusm.edu>
2019-11-13 13:43:47 -0800

    Added routes to create class and task

--- app/src/main/java/csusm/cs443/cougarplanner/ViewMyClasses.kt
+++ app/src/main/java/csusm/cs443/cougarplanner/ViewMyClasses.kt
@@ -1,12 +1,18 @@
 package csusm.cs443.cougarplanner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_my_classes.*
 
 class ViewMyClasses : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_my_classes)
+
+        addClassBtn.setOnClickListener {
+            startActivity(Intent(this, CreateNewClass::class.java))
+        }
     }
 }
